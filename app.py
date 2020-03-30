f0e9a41598fbc77c80399fca782369bb331ce801
Shubham Kapoor <31004156+shukapoo@users.noreply.github.com>
2020-03-30 16:39:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # EDIT THE FOLLOWING LINE
-DefaultTitle="Work in progress by shukapoo"
+DefaultTitle="Work in progress by shukapoo on FMI course"
 
 # Don't touch the code below unless you really mean to.
 
